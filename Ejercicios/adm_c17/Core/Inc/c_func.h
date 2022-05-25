f8afb8092b01d80fc2fa13755468359cc748ce1a
Jhonny Velasco <javelasco@outlook.com>
2022-05-24 22:02:18 -0500

    Se sube el ejercicio 1 en C

---
+++ Ejercicios/adm_c17/Core/Inc/c_func.h
@@ -0,0 +1,16 @@
+ * c_func.h
+ *
+ *  Created on: May 24, 2022
+ *      Author: jvelasco
+ */
+
+#include <stdio.h>
+
+#ifndef INC_C_FUNC_H_
+#define INC_C_FUNC_H_
+
+
+void zeros(uint32_t vector, uint32_t longitud);
+
+
+#endif /* INC_C_FUNC_H_ */
---
+++ Ejercicios/adm_c17/Core/Src/c_func.c
@@ -0,0 +1,8 @@
+/*
+ * c_func.c
+ *
+ *  Created on: May 24, 2022
+ *      Author: jvelasco
+ */
+
+
